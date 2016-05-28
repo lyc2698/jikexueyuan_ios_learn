21f4306ddedbd9e74fdcb1b1315cb9232023c23a
lyc2698 <lyc2698@qq.com>
2016-05-29 01:59:59 +0800

    完成 UIButton 作业

--- useUIButton/useUIButton/ViewController.m
+++ useUIButton/useUIButton/ViewController.m
@@ -23,17 +23,24 @@
     [super viewDidLoad];
     
     //创建按钮
-//    UIButton *btn = [[UIButton alloc]init];
-//    btn.frame = CGRectMake(0, 200, 150, 60);
-//    btn.backgroundColor = [UIColor redColor];
-//    [btn setImage:[UIImage imageNamed:@"1.jpg"] forState:UIControlStateNormal];
-//    [btn setImage:[UIImage imageNamed:@"2.jpg"] forState:UIControlStateHighlighted];
-//    [btn setTitle:@"默认" forState:UIControlStateNormal];
-//    [btn setTitle:@"高亮" forState:UIControlStateHighlighted];
-//    
-//    [self.view addSubview:btn];
+    UIButton *btn = [[UIButton alloc]init];
+    //设置位置
+    btn.frame = CGRectMake(10, 50, 150, 60);
+    //设置字体颜色
+    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    //设置图片内容
+    [btn setImage:[UIImage imageNamed:@"1.jpg"] forState:UIControlStateNormal];
+    [btn setImage:[UIImage imageNamed:@"2.jpg"] forState:UIControlStateHighlighted];
+    //设置文字内容
+    [btn setTitle:@"默认" forState:UIControlStateNormal];
+    [btn setTitle:@"高亮" forState:UIControlStateHighlighted];
+    //设置文字和图片位置
+    [btn setImageEdgeInsets:UIEdgeInsetsMake(0, 50, 0, 0)];
+    [btn setTitleEdgeInsets:UIEdgeInsetsMake(0, -350, 0, 0)];
+    //添加视图
+    [self.view addSubview:btn];
     
-    
+    /** 按钮操作. */
     //设置选中状态时候的图片
     [self.selectBtn addTarget:self action:@selector(selectBtn:) forControlEvents:UIControlEventTouchUpInside];
     //设置禁用状态时候的图片
@@ -46,17 +53,22 @@
 /** 设置选中状态时候的图片. */
 - (void)selectBtn:(UIButton*)btn {
     [self.myBtn setImage:[UIImage imageNamed:@"1.jpg"] forState:UIControlStateSelected];
+    self.myBtn.enabled = YES;
+    self.myBtn.selected = YES;
 }
 
 /** 设置禁用状态时候的图片. */
 - (void)disableBtn:(UIButton*)btn {
     [self.myBtn setImage:[UIImage imageNamed:@"3.jpg"] forState:UIControlStateDisabled];
+    self.myBtn.enabled = NO;
+    self.myBtn.selected = NO;
 }
 
 /** 设置普通状态时候的图片. */
 - (void)normalBtn:(UIButton*)btn {
     [self.myBtn setImage:[UIImage imageNamed:@"2.jpg"] forState:UIControlStateNormal];
-    [self.myBtn setTitle:@"默认" forState: UIControlStateSelected];
+    self.myBtn.enabled = YES;
+    self.myBtn.selected = NO;
 }
 
 @end
